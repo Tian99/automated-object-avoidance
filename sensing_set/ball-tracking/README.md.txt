fabfad9d809272bb04b8b837c4e40ff70697f61f
Tian99 <jit29@pitt.edu>
2019-07-21 04:58:06 -0400

    data collection mode completed, data analysis next

--- sensing_set/ball-tracking/README.md.txt
+++ sensing_set/ball-tracking/README.md.txt
@@ -1,1 +1,8 @@
-Starting the project: automated avoidance
+data collection mode:sudo python main.py --collect
+enter 1,2,or 3 during the process to choose which file to written to
+press space to complete a complete movement. 
+
+the terminal command: --collect: Run the script to collect all appropriate data for later analysis
+attention: the --collect mode must be run under sudo.
+
+
--- sensing_set/ball-tracking/collection.py
+++ sensing_set/ball-tracking/collection.py
@@ -1,5 +1,52 @@
+#Must be run under sudo mode
+#cd into ball_tracking file before executing
+import keyboard
+import json
+
+#Create three different file for different collision possibilities
+#first file
+address_one = "./collision_set/collision_ensured.txt"
+address_two = "./collision_set/miss_ensured.txt"
+address_three = "./collision_set/unsure.txt"
+
 #Define the function to collect all the data
+file_one = open(address_one,"w+")
+file_two = open(address_two,"w+")
+file_three = open(address_three,"w+")
+
+def written_process(dict, file_type):
+    #Write the data to the file
+    file_type.write(json.dumps(dict))
+    file_type.write('\n')
+    #One space key pressed means one data set
+    if keyboard.is_pressed(' '):
+        #Formatter
+        file_type.write('-----------------------------------------------\n\n')
+            
+    #file_one.close()
+file_choice = None
 def collection(dX,dY,x,y,moving_time):
-    #Create three different file for different collision possibilities
-    #first file
-    print("implemented later")
+    global file_choice
+    dict = {}
+    #Construct a library
+    dict['dX']=dX
+    dict['dY']=dY
+    dict['x']=x
+    dict['y']=y
+    dict['moving_time']=moving_time
+    
+    #Convert the dictionary to string
+    dict={'dict':dict}
+    #Key input to decide which file it's gonna written to
+    if keyboard.is_pressed(2):
+        print('written to file 1')
+        file_choice = file_one
+    elif keyboard.is_pressed(3):
+        print('written to file 2')
+        file_choice = file_two
+    elif keyboard.is_pressed(4):
+        print('written to file 3')
+        file_choice = file_three
+    #Written specific data to specific files
+    if file_choice is not None:
+        written_process(dict, file_choice)
