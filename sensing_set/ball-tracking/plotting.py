44d95456fcfd7cd7b9c5c41fcacdfc86b6bcee84
Tian99 <jit29@pitt.edu>
2019-07-23 19:54:38 -0400

    Plotting finished, more features to add

---
+++ sensing_set/ball-tracking/plotting.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import json
+
+def data_loading(address):
+    #Define an empty list
+    data_total = list()
+    #Load data in file
+    file = open(address)
+    #Write to a dictionary
+    for line in file.readlines():
+        #Append each dictionary to a list
+        data_total.append(json.loads(line))
+        
+            
+    return data_total
+
+def plot_action(data_number):
+    x_number = []
+    y_number = []
+    if data_number is not None:
+        for i in data_number:
+            x_number.append(i['x'])
+            y_number.append(i['y'])
+        plt.plot(x_number,y_number)
+        plt.show()
+
+def plot():
+    #First read file
+    address_one = "./collision_set/collision_ensured.txt"
+    address_two = "./collision_set/miss_ensured.txt"
+    address_three = "./collision_set/unsure.txt"
+    
+    #Pass the list containing each dictionary
+    data_one = data_loading(address_one)
+    data_two = data_loading(address_two)
+    data_three = data_loading(address_three)
+    
+    plot_action(data_one)
+    plot_action(data_two)
+    plot_action(data_three)
+    
+    #print(data_one)
+    #print(data_two)
+    #print(data_three)
+        
+    #print(collision_ensured)
+
+#plotting()
